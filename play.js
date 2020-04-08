8022360f4372da7af560878874a2a15a87ab2c62
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-08 17:57:40 +0000

    refactoring to make play and connection modular

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-
-  conn.on('data', data => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+const connection = require('./client');;
 console.log('Connecting ...');
 
-const conn = connect();
+const conn = connection();
