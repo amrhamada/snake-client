a6710b0f9337558a941cae32ccb32a4a812806e0
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-08 20:14:41 +0000

    added key handler

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const connection = require('./client');;
 console.log('Connecting ...');
-
 const conn = connection();
+conn.setEncoding('utf8');
+
+const setupInput = (callback) => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => handleUserInput(data));
+  return stdin;
+
+};
+
+const handleUserInput = (data) => {
+  if(data === '\u0003') {
+    process.exit();
+  }
+  
+};
+
+stdin = setupInput();
