716fb3f3266ec7f961a756b73889d9abcc74f8ca
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-08 20:19:33 +0000

    something

--- play.js
+++ play.js
@@ -1,23 +1,9 @@
-const connection = require('./client');;
+const connection = require('./client');
 console.log('Connecting ...');
 const conn = connection();
 conn.setEncoding('utf8');
+const setupInput = require('./input');
 
-const setupInput = (callback) => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => handleUserInput(data));
-  return stdin;
 
-};
-
-const handleUserInput = (data) => {
-  if(data === '\u0003') {
-    process.exit();
-  }
-  
-};
 
 stdin = setupInput();
